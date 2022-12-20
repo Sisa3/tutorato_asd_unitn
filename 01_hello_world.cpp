aec943fce04f490bddcdc140848c9934e68f7c70
Sisa3 <74955506+Sisa3@users.noreply.github.com>
2022-12-20 18:38:09 +0100

    Create 01_hello_world.cpp

---
+++ 01_hello_world.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// First Hello World in c++
+
+int main() {
+    cout << "Hello world!";
+    return 0;
+}
+
+
+/*   
+* 	 PER COMPILARE:
+*    g++ 01_hello_world.cpp
+*        crea un eseguibile chiamato a.exe oppure a.out
+*    g++ 01_hello_world.cpp -o 01_hello_world
+*        crea un eseguibile chiamato 01_hello_world.exe o 01_hello_world.out)
+*   
+*   PER ESEGUIRE:
+*   ./01_hello_world.exe
+*
+*/

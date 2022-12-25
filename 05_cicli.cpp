7113b21afebef38a2440d9d6e7492bbfdbf10efe
Sisa3 <74955506+Sisa3@users.noreply.github.com>
2022-12-25 23:52:36 +0100

    Update 05_cicli.cpp

--- 05_cicli.cpp
+++ 05_cicli.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 
-	*/
+	/*
 	*	Ciclo WHILE:
 	*	esegui una certa azione finche' la condizione rimane vera
 	*/
@@ -14,8 +14,18 @@ int main(){
 		a--;
 	}
     cout << "Ora a = " << a << endl;
+	
+	// Metodo alternativo per costruire i cicli:
+    vector<string> message {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    for (string word : message) {
+        cout << word << " ";
+    }    
+	cout << endl;
+	// Stampa tutte le stringhe che compongono il vector message
 
-	*/
+    
+
+	/*
 	*	Ciclo FOR:
 	*	esegui una certa azione,, iterando un contatore in un certo range
 	*/
